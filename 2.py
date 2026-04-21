817449b2d6112a02cdba78187f8561e244e91ead
Sushma.S <suma4473@gmail.com>
2026-04-21 07:00:38 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,42 @@
+a=10
+b=5
+print(a+b)
+a=b=c=d=12
+print(a)
+print(b)
+print(c)
+print(d)
+
+name='Sushma'
+Name="Rohit"
+print(name)
+print(Name)
+x=1000
+y=20
+print(x+y)
+_Name="sushma"
+print(_Name)
+is_Student=True # bool
+weight=56.1 #float
+age=21
+print(type(age))
+print(type(weight))
+print(type(is_Student))
+print(type(_Name))
+is_Student="no"
+print(type(is_Student))
+
+#Type cast
+age=float(age)
+print(age)
+print(type(age))
+s="100"
+print((type(s)))
+s=int(s)
+print(s+age)
+print(x-y)
+print(x%y)
+print(x/y)
+print(x//y)
+print(x*y)
+print(x**y)
