4ee623640db6177e18f9e39ca393fa0b919c7bb5
Sushma.S <suma4473@gmail.com>
2026-04-26 22:00:40 +0530

    added operators concept

---
+++ Operators.py
@@ -0,0 +1,35 @@
+'''a=10
+b=10
+a+=100
+b*=20#a=a*20
+a-=5 
+b/=10
+
+print(a)
+print(b)
+#Comparison operator
+print(a<=b)
+print(a>=b)
+print(a==b)
+
+
+print(a!=b)'''
+
+#Logical operator
+s="chandan"
+s2="SushmaSc"
+print(('c' in s) and ('c' in s2))
+print(('c' in s) and ('c' not in s2))
+print(('c' in s) or ('c' not in s2))
+print(not('g' in s))
+#Bitwise operator
+a=5
+b=3
+print(a&b)
+print(a or b)
+print(a | b)#xor
+print(a^b)
+print(~b)#not
+print(~a) #not
+print(a<<1)#shift left 1
+print(a>>1)#shift right
