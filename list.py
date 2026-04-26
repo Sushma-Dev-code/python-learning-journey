7a9afae48f6bea4c848bc0f99515bc76bb849bb0
Sushma.S <suma4473@gmail.com>
2026-04-26 21:59:05 +0530

    added List concept

---
+++ list.py
@@ -0,0 +1,20 @@
+item=['bru','sugar','salt','fruits']
+print(item)
+print(item[2])
+print(item[-3])
+item.pop() #remove last item
+print(item)
+item.append("curd")
+print(item)
+item.remove('sugar')
+print(item)
+item.insert(1,"chilli powder")
+print(item)
+item[0]="sunrises"
+print(len(item))
+l=[100,200,400,300,500]
+l2=l[0::2]
+print(l[0::2])
+print(l2)
+number=[1,2,"sushma",'python']
+print(sum(number))#not possible to different types of datatypes
