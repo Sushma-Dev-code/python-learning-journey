3b39f3d3a9274f8bf2a829d97dcc038f24b84960
Sushma.S <suma4473@gmail.com>
2026-04-27 21:14:47 +0530

    updated tuples and set

---
+++ Tuples and Set.py
@@ -0,0 +1,21 @@
+genders=("female","male","others")
+print(genders)
+print(type(len(genders)))
+print(type(genders))
+print(genders[1::2])
+print(genders[0:3])
+
+s={8,2,3,7}
+s1={2,3,4,5,7,8}
+print(s and s1)
+a=set()
+print(type(a))
+
+
+print(s|s1)
+print(s&s1)
+
+print(s^s1)
+print(s-s1)
+
+
---
+++ While.py
@@ -0,0 +1,13 @@
+#while
+#condition=True
+#while condition:
+   # print("condition is true")
+    #break
+i=0
+while i<=10:
+    x=0
+    while x<i:
+        print("sushma")
+        x +=1
+    print(i)
+    i+=1
