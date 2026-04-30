8cb22a1e2f3b5272d28649494831868ba87198fa
Sushma.S <suma4473@gmail.com>
2026-04-30 23:06:57 +0530

    Added String

---
+++ String.py
@@ -0,0 +1,26 @@
+first_Name="Romeo"
+Last_Name="juliet"
+full_Name=first_Name+ " " +Last_Name
+print(first_Name+ " " +Last_Name)
+print(full_Name)
+
+
+Greeting="Hello!"*3
+print(Greeting)
+
+Message="india is beautifull!  "
+print(Message)
+print(Message.upper())
+print(Message.lower())
+print(Message.strip()*3)
+print(Message.replace("beautifull", "Amazing"))
+name='handan says "hello" darshan said"hi"'
+print(len(name))
+Name="Sushma"
+print(len(Name))
+print(Name[3])
+print(Name[2 :4])
+print(Name[2 :])
+print(Name[:6])
+print(Name[-2:])
+print(Name[::2])
