da1dd0f5c31997cc0bac1fb20ec0caaeb5e37b9f
Sushma.S <suma4473@gmail.com>
2026-05-09 23:24:21 +0530

    Add files via upload

---
+++ Variable .py
@@ -0,0 +1,15 @@
+# A variable is a name you create to store a value. so you can use it later in your code 
+a=12
+b=4
+c=a+b
+d=a*b
+e=a%b
+f=a/b
+g=a-b
+h=a^b
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(c)
